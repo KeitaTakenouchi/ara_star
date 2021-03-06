66508e291eb07c4ddf46f7a290d3882657308334
Takenouchi Keita <t.keita.osaka@gmail.com>
2021-03-07 03:01:46 +0900

    Refactor the entire code.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,52 +2,50 @@ import java.util.*
 import kotlin.math.abs
 
 fun main() {
-    val area = Area(Node(0, 0), Node(13, 19)).apply {
+    val grid = Grid(P(0, 0), P(13, 19)).apply {
         print()
     }
-    Search(area).execute()
+    Search(grid).execute(weight = 2.0)
 }
 
-class Search(val area: Area) {
-    fun execute() {
-        val weight = 1.0
+class Search(val grid: Grid) {
+    fun execute(weight: Double) {
         val costs = weightedAsterCost(weight)
         if (costs == null) {
             println("unreachable")
             return
         }
         val path = resolvePath(costs)
-        area.print(path)
-        area.printPathAsNumpy(path)
-        area.printCostAsNumpy(costs)
+        grid.print(path)
+        grid.printPathAsNumpy(path)
+        grid.printCostAsNumpy(costs)
     }
 
-    private fun resolvePath(costs: Map<Node, Int>): Path {
-        val path = Path().apply { addNode(area.goal) }
-        var target = area.goal
-        while (target != area.start) {
-            val next = area.neighbors(target)
-                .filter { costs.containsKey(it) }
-                .minByOrNull { costs[it]!! }!!
-            target = next
-            path.addNode(target)
+    private fun resolvePath(costs: Map<P, Int>): Path {
+        val path = Path()
+        var target = grid.goal
+        while (target != grid.start) {
+            path.addPoint(target)
+            target = grid.neighbors(target)
+                .minByOrNull { costs.getOrDefault(it, Int.MAX_VALUE) }!!
         }
         return path
     }
 
-    private fun weightedAsterCost(weight: Double): Map<Node, Int>? {
-        val h = { n: Node -> weight * n.dist(area.goal) }
+    private fun weightedAsterCost(weight: Double): Map<P, Int>? {
+        // h: heuristic function
+        val h = { n: P -> weight * n.distance(grid.goal) }
 
-        val costs = mutableMapOf<Node, Int>().apply { this[area.start] = 0 }
-        val queue = PriorityQueue<NodeP>().apply { add(NodeP(area.start, h(area.start))) }
+        val costs = mutableMapOf<P, Int>().apply { this[grid.start] = 0 }
+        val queue = PriorityQueue<PC>().apply { add(PC(grid.start, h(grid.start))) }
         while (queue.isNotEmpty()) {
-            val target = queue.poll().node
-            if (target == area.goal) return costs
-            for (n in area.neighbors(target)) {
-                val cost = costs[target]!! + target.dist(n)
+            val p = queue.poll().point
+            if (p == grid.goal) return costs
+            grid.neighbors(p).forEach { n ->
+                val cost = costs[p]!! + p.distance(n)
                 if (costs[n] == null || cost < costs[n]!!) {
                     costs[n] = cost
-                    queue.add(NodeP(n, costs[n]!! + h(n)))
+                    queue.add(PC(n, cost + h(n)))
                 }
             }
         }
@@ -55,8 +53,8 @@ class Search(val area: Area) {
     }
 }
 
-class NodeP(val node: Node, private val cost: Double) : Comparable<NodeP> {
-    override fun compareTo(other: NodeP): Int {
+class PC(val point: P, private val cost: Double) : Comparable<PC> {
+    override fun compareTo(other: PC): Int {
         return when {
             cost == other.cost -> 0
             cost < other.cost -> -1
@@ -67,13 +65,13 @@ class NodeP(val node: Node, private val cost: Double) : Comparable<NodeP> {
 }
 
 class Path {
-    val nodes = mutableListOf<Node>()
-    fun addNode(node: Node) {
-        nodes.add(node)
+    val points = mutableListOf<P>()
+    fun addPoint(point: P) {
+        points.add(point)
     }
 }
 
-class Area(val start: Node, val goal: Node) {
+class Grid(val start: P, val goal: P) {
     val states = arrayOf(
         arrayOf(0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0),
         arrayOf(0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0),
@@ -91,21 +89,29 @@ class Area(val start: Node, val goal: Node) {
         arrayOf(0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
     )
 
-    fun isValid(n: Node): Boolean {
+    fun isValid(n: P): Boolean {
         return (n.i in states.indices) && (n.j in states[n.i].indices)
     }
 
-    fun state(n: Node): Int {
+    fun state(n: P): Int {
         check(isValid(n)) { "out of range $n" }
         return states[n.i][n.j]
     }
 
-    fun isEmpty(n: Node): Boolean {
+    fun isEmpty(n: P): Boolean {
         return state(n) == 0
     }
 
-    fun neighbors(n: Node): List<Node> {
+    fun neighbors(n: P): List<P> {
         return n.around().filter { isValid(it) && isEmpty(it) }
+    }
+
+    fun costMatrix(costs: Map<P, Int>): Array<Array<Int>> {
+        return states.mapIndexed { i, row ->
+            row.mapIndexed { j, _ ->
+                costs.getOrDefault(P(i, j), -30)
+            }.toTypedArray()
+        }.toTypedArray()
     }
 
     fun print(path: Path = Path()) {
@@ -116,9 +122,9 @@ class Area(val start: Node, val goal: Node) {
             for (j in states[i].indices) {
                 val s = states[i][j]
                 val c = when {
-                    Node(i, j) == start -> " S "
-                    Node(i, j) == goal -> " G "
-                    path.nodes.contains(Node(i, j)) -> " @ "
+                    P(i, j) == start -> " S "
+                    P(i, j) == goal -> " G "
+                    path.points.contains(P(i, j)) -> " @ "
                     s == 0 -> "   "
                     s == 1 -> " * "
                     else -> throw IllegalStateException("unknown state $s")
@@ -131,24 +137,16 @@ class Area(val start: Node, val goal: Node) {
         print(sb.toString())
     }
 
-    fun costMatrix(costs: Map<Node, Int>): Array<Array<Int>> {
-        return states.mapIndexed { i, row ->
-            row.mapIndexed { j, _ ->
-                costs.getOrDefault(Node(i, j), -30)
-            }.toTypedArray()
-        }.toTypedArray()
-    }
-
     fun printPathAsNumpy(path: Path) {
         var i = 0
-        val m = states.joinToString(",\n") { _ ->
+        val m = states.joinToString(",\n") {
             var j = 0
-            val r = states[i].joinToString(",") { _ ->
+            val r = states[i].joinToString(",") {
                 val s = states[i][j]
                 val str = when {
-                    Node(i, j) == start -> "  0"
-                    Node(i, j) == goal -> "  0"
-                    path.nodes.contains(Node(i, j)) -> " 30"
+                    P(i, j) == start -> "  0"
+                    P(i, j) == goal -> "  0"
+                    path.points.contains(P(i, j)) -> " 30"
                     s == 0 -> "-30"
                     s == 1 -> " 60"
                     else -> throw IllegalStateException("unknown state $s")
@@ -162,12 +160,12 @@ class Area(val start: Node, val goal: Node) {
         println("data = np.array([\n$m]\n)")
     }
 
-    fun printCostAsNumpy(costs: Map<Node, Int>) {
+    fun printCostAsNumpy(costs: Map<P, Int>) {
         val costMatrix = costMatrix(costs)
         var i = 0
-        val m = states.joinToString(",\n") { _ ->
+        val m = states.joinToString(",\n") {
             var j = 0
-            val r = states[i].joinToString(",") { _ ->
+            val r = states[i].joinToString(",") {
                 val str = when {
                     states[i][j] == 1 -> "60"
                     else -> costMatrix[i][j].toString().padStart(3, ' ')
@@ -182,30 +180,31 @@ class Area(val start: Node, val goal: Node) {
     }
 }
 
-data class Node(val i: Int, val j: Int) {
-    fun around(): Array<Node> {
+// Point class
+data class P(val i: Int, val j: Int) {
+    fun around(): Array<P> {
         return arrayOf(up(), left(), down(), right())
     }
 
     // Manhattan distance
-    fun dist(o: Node): Int {
+    fun distance(o: P): Int {
         return abs(i - o.i) + abs(j - o.j)
     }
 
-    fun up(): Node {
-        return Node(i - 1, j)
+    fun up(): P {
+        return P(i - 1, j)
     }
 
-    fun down(): Node {
-        return Node(i + 1, j)
+    fun down(): P {
+        return P(i + 1, j)
     }
 
-    fun left(): Node {
-        return Node(i, j - 1)
+    fun left(): P {
+        return P(i, j - 1)
     }
 
-    fun right(): Node {
-        return Node(i, j + 1)
+    fun right(): P {
+        return P(i, j + 1)
     }
 
     override fun toString(): String {
